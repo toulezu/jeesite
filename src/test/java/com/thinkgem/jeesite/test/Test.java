34d4aab064fafa925f2af3bbbaf9ed9f8dee6330
ck <svsechen@163.com>
2017-05-19 19:03:38 +0800

    新增分支 cms_deepmeiju

--- src/test/java/com/thinkgem/jeesite/test/Test.java
+++ src/test/java/com/thinkgem/jeesite/test/Test.java
@@ -1,8 +1,15 @@
 package com.thinkgem.jeesite.test;
 
-public class Test {
+import java.util.Random;
 
+public class Test {
+	private static final String[] wins = { "10.3.6.28" };
+	private static final String[] macs = { "DST61858","DST61857","DST61859", };
+	
 	public static void main(String[] args) {
+		
+		Random ran = new Random();
+		System.out.println(ran.nextInt(wins.length));
 		
 	}
 }
